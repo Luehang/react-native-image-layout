1e3402fad349e21acdf6c04721d64c19472fe983
Lue <lue_hang@hotmail.com>
2019-07-25 13:41:36 -0500

    Update example project.

--- example/index.js
+++ example/index.js
@@ -3,3 +3,4 @@ import ReactNativeImageLayoutExample from "./ReactNativeImageLayoutExample";
 import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => ReactNativeImageLayoutExample);
+
